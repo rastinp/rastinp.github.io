92378ce607028e9da9f4304ded9d1772fb9fe5ef
rastinp <70774892+rastinp@users.noreply.github.com>
2021-01-01 14:04:58 +0100

    Update app.js

--- webxr/start/lecture3_10/app.js
+++ webxr/start/lecture3_10/app.js
@@ -133,12 +133,21 @@ class App{
         }
         
         function onSessionStart(){
-            
+            self.ui.mesh.position.set80, -0.5, -1.1);
+            self.camera.add( self.ui.mesh );
         }
         
         function onSessionEnd(){
-            
+            self.camera.remove( self.ui.mesh );
         }
+        
+        const btn = new ARButton( this.renderer, { onSessionStart, onSessionEnd, sessionInit: { optionalFeatures: [ 'dom-overlay'], domOverlay: { root: document.body }} });
+        
+        const controller = this.renderer.xr.getController(0);
+        controller.addEventListener( 'connected', onConnected);
+        
+        this.scene.add( controller );
+        this.controller = controller;
         
         this.renderer.setAnimationLoop( this.render.bind(this) );
     }
