e8df279d3f99afd31b25a5d9eb5215f0fc79c46c
rastinp <70774892+rastinp@users.noreply.github.com>
2021-03-01 09:29:18 +0100

    test

--- webxr/complete/rastin/app.js
+++ webxr/complete/rastin/app.js
@@ -76,7 +76,7 @@ class App{
 		// Load a GLTF resource
 		loader.load(
 			// resource URL rastin_5 works
-			`rastin_6.glb`,
+			`rastin_7.glb`,
 			// called when the resource is loaded
 			function ( gltf ) {
 				const object = gltf.scene.children[3];
